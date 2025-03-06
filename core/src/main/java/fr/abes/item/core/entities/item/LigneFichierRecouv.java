efb16d57eed2044060d5b61a4f3e797fa8d8e40e
SamuelQuetin <samquetinuel@gmail.com>
2025-03-06 09:59:16 +0100

    supp column Definition TEXT

--- core/src/main/java/fr/abes/item/core/entities/item/LigneFichierRecouv.java
+++ core/src/main/java/fr/abes/item/core/entities/item/LigneFichierRecouv.java
@@ -13,7 +13,7 @@ import java.io.Serializable;
 @NoArgsConstructor
 @Getter @Setter
 public class LigneFichierRecouv extends LigneFichier implements Serializable, ILigneFichier {
-    @Column(name = "INDEX_RECHERCHE", columnDefinition = "TEXT")
+    @Column(name = "INDEX_RECHERCHE")
     private String indexRecherche;
 
     @Column(name = "NB_REPONSES")
