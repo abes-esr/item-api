82e8b25c4fabcf638cea7ff51f514fadfdae4b8c
William Donnette <willdonn17@gmail.com>
2025-07-16 11:36:10 +0200

    Gestion des espaces dans les fichiers initiaux de ppn et epn dans le cas d'espace introduits apres un ppn et avant le ppn suivant lors d'une suppression + Gestion des 0

--- core/src/main/java/fr/abes/item/core/components/FichierPrepare.java
+++ core/src/main/java/fr/abes/item/core/components/FichierPrepare.java
@@ -112,12 +112,13 @@ public class FichierPrepare extends AbstractFichier implements Fichier {
 			String[] tabEpn = listeEpn.split(",");
 			Multimap<String, String> resJson = Utilitaires.parseJson(input, true);
 			for (String epn : tabEpn) {
-				if (resJson.containsKey(epn)) {
-					for (String ppn : resJson.get(epn)) {
-						out.println(ppn + ";" + rcr + ";" + epn + ";");
+				String epnFormatted = String.format("%9s", epn).replace(' ', '0').trim();
+				if (resJson.containsKey(epnFormatted)) {
+					for (String ppn : resJson.get(epnFormatted)) {
+						out.println(ppn + ";" + rcr + ";" + epnFormatted + ";");
 					}
 				} else
-					out.println(";" + rcr + ";" + epn + ";");
+					out.println(";" + rcr + ";" + epnFormatted + ";");
 			}
 		} catch (IOException ex) {
 			log.error(Constant.ERROR_UNABLE_TO_CREATE_FILE);
